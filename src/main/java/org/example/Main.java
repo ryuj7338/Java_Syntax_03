caf231eea67961519e61fbc4a24ae5012a23014d
ryuj7338 <ryuj7338@gmail.com>
2025-03-04 20:08:14 +0900

    커밋 테스트3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,10 @@
 package org.example;
 
- // 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 조건 : System.out.println은 딱 한번만 사용 가능
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("1.안녕하세요.");
-        System.out.println("2.안녕하세요.");
-        System.out.println("3.안녕하세요.");
-        System.out.println("4.안녕하세요.");
-        System.out.println("5.안녕하세요.");
-        System.out.println("6.안녕하세요.");
-        System.out.println("7.안녕하세요.");
-        System.out.println("8.안녕하세요.");
-        System.out.println("9.안녕하세요.");
-        System.out.println("10.안녕하세요.");
-
+        System.out.println("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.");
     }
 }
