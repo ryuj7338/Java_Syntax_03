872c3b5830d6344fd1b7c2646f733b922a9960c2
ryuj7338 <ryuj7338@gmail.com>
2025-03-04 20:03:04 +0900

    커밋 테스트

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // 프로그램의 시작점
+        System.out.println("Hello world!");
+        // 프로그램의 끝점
+    }
+}
