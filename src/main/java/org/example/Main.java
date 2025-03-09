a2491f11a28378cc235806927ff6b31793e5fc4c
ryuj7338 <ryuj7338@gmail.com>
2025-03-09 22:03:06 +0900

    별찍기 해석

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,135 +1,40 @@
 package org.example;
 
-//// 문제 : 아래와 같이 출력해주세요.
-//
-//class Main {
-//    public static void main(String[] args) {
-//        System.out.println("8 * 1 = 8");
-//        System.out.println("8 * 2 = 16");
-//        System.out.println("8 * 3 = 24");
-//        System.out.println("8 * 4 = 32");
-//        System.out.println("8 * 5 = 40");
-//        System.out.println("8 * 6 = 48");
-//        System.out.println("8 * 7 = 56");
-//        System.out.println("8 * 8 = 64");
-//        System.out.println("8 * 9 = 72");
-//
-//        // 출력
-//    /*
-//    8 * 1 = 8
-//    8 * 2 = 16
-//    8 * 3 = 24
-//    8 * 4 = 32
-//    8 * 5 = 40
-//    8 * 6 = 48
-//    8 * 7 = 56
-//    8 * 8 = 64
-//    8 * 9 = 72
-//    */
-//    }
-//}
 
 
-
-//// 문제 : 아래와 같이 출력해주세요.
-//// 문제 : dan 변수의 값에 따라 첫번째로 곱하는 수가 변하게 해주세요.
-//
-//class Main {
-//    public static void main(String[] args) {
-//        int dan = 8;
-//
-//        System.out.println(dan+" * 1 = 8");
-//        System.out.println(dan+" * 2 = 16");
-//        System.out.println(dan+" * 3 = 24");
-//        System.out.println(dan+" * 4 = 32");
-//        System.out.println(dan+" * 5 = 40");
-//        System.out.println(dan+" * 6 = 48");
-//        System.out.println(dan+" * 7 = 56");
-//        System.out.println(dan+" * 8 = 64");
-//        System.out.println(dan+" * 9 = 72");
-//
-//        // 출력
-//    /*
-//    8 * 1 = 8
-//    8 * 2 = 16
-//    8 * 3 = 24
-//    8 * 4 = 32
-//    8 * 5 = 40
-//    8 * 6 = 48
-//    8 * 7 = 56
-//    8 * 8 = 64
-//    8 * 9 = 72
-//    */
-//    }
-//}
-
-
-//// 문제 : 아래와 같이 출력해주세요.
-//// 문제 : dan 변수의 값에 따라 그에 해당하는 구구단이 출력 되도록 해주세요.
-//
-//class Main {
-//    public static void main(String[] args) {
-//        int dan = 9;
-//
-//        System.out.println(dan + " * 1 = " +dan * 1);
-//        System.out.println(dan + " * 2 = " +dan * 2);
-//        System.out.println(dan + " * 3 = " +dan * 3);
-//        System.out.println(dan + " * 4 = " +dan * 4);
-//        System.out.println(dan + " * 5 = " +dan * 5);
-//        System.out.println(dan + " * 6 = " +dan * 6);
-//        System.out.println(dan + " * 7 = " +dan * 7);
-//        System.out.println(dan + " * 8 = " +dan * 8);
-//        System.out.println(dan + " * 9 = " +dan * 9);
-//
-//        // 출력
-//    /*
-//    8 * 1 = 8
-//    8 * 2 = 16
-//    8 * 3 = 24
-//    8 * 4 = 32
-//    8 * 5 = 40
-//    8 * 6 = 48
-//    8 * 7 = 56
-//    8 * 8 = 64
-//    8 * 9 = 72
-//    */
-//    }
-//}
-
-
-// 문제 : 아래와 같이 출력해주세요.
-// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
-
-class Main {
+public class Main {
     public static void main(String[] args) {
-        int dan = 8;
 
-        // 수정가능지역 시작
-        int i = 0;
-        i += 1;
+        //        int rows = 4;
+        //
+        //        for (int i = 1; i <= rows; i++) { // i는 row가 4니까 i는 1부터 4까지 4번 출력
+        //            for (int k = 1; k <= i; k++) { // k도 i가 4까지 출력이니까 4번 출력
+        //                System.out.print("* ");
+        //            }   // k가 1일때 i도 1이므로 한 번 줄바꿈없이 출력후 i에 해당하는 for문으로 빠져나감.
+        //            System.out.println();   // "*"넣으면 "*"추가되면서 줄바꿈됨.
+        //        }   // i에 해당하는 for문은 줄바꿈하면서 출력
 
-        System.out.println(dan + " * " + i + " = " + dan * 1);
-        System.out.println(dan + " * 2 = " + dan * 2);
-        System.out.println(dan + " * 3 = " + dan * 3);
-        System.out.println(dan + " * 4 = " + dan * 4);
-        System.out.println(dan + " * 5 = " + dan * 5);
-        System.out.println(dan + " * 6 = " + dan * 6);
-        System.out.println(dan + " * 7 = " + dan * 7);
-        System.out.println(dan + " * 8 = " + dan * 8);
-        System.out.println(dan + " * 9 = " + dan * 9);
-        // 수정가능지역 끝
 
-        // 출력
-    /*
-    8 * 1 = 8
-    8 * 2 = 16
-    8 * 3 = 24
-    8 * 4 = 32
-    8 * 5 = 40
-    8 * 6 = 48
-    8 * 7 = 56
-    8 * 8 = 64
-    8 * 9 = 72
-    */
+        int n = 3;      // 오직 정수로만 이루어져 있는 n이라는 변수에 3을 넣겠다.
+
+        for(int i = 1; i <= n; i++) {   // 오직 정수로만 이루어져 있는 i라는 변수에 1을 넣겠다. i가 1씩 증가하면서 n(= 3)까지 반복문을 실행하겠다.
+            for (int j = i; j < n; j++) {   // 오직 정수로만 이루어져 있는 j라는 변수에 i값을 넣겠다.
+                System.out.print(" ");      // j가 n까지 즉, 3까지 도달할 때까지 " "를 반복한 후 j가 3이 되면 for문에서 빠져나가겠다.
+            }
+            for (int k = 1; k <= (2 * i -1); k++){  // 정수로만 이루어져 있는 k에 1을 넣겠다. k가 (2 * i -1)까지 1씩 증가하면서 반복문을 실행하겠다.
+                System.out.print("*");      // k의 반복문이 실행될때 동안 "*"을 출력하겠다.
+            }                   // k가 (2 * i -1)까지 도달하면 for문을 빠져나간다.
+            System.out.println();       // j의 for문과 k의 for문이 다 실행되면 출력을 하겠다.
+        }   // i의 for문이 다 실행되면 빠져나간다.
+
+        for(int i = n -1 ; i >= 1; i--) {   // i라는 변수에 n - 1 (즉, 2)을 넣겠다. i가 1씩 감소하면서 1이 될 때까지 반복문을 실행하겠다.
+            for (int k = n; k > i; k--) {   // k라는 변수에  n(즉, 3)을 넣겠다. k가 i(즉, 2)가 될 때까지 1씩 감소하면서 반복문을 실행하겠다.
+                System.out.print(" ");      // k가 i가 될 때까지 " "을 출력하겠다.
+            }   // k가 i가 되면 i 반복문을 빠져나간다.
+            for (int k = 1; k <= (2 * i -1); k++){  // k라는 변수에 1을 넣겠다. k가 (2 * i - 1)까지 1씩 증가하면서 반복문을 실행하겠다.
+                System.out.print("*");  // k가 (2 * i - 1)이 될 때까지 "*"를 출력하겠다.
+            }   // k가 (2 * i - 1)이 되면 반복문을 빠져나간다.
+            System.out.println();   //  i가 n -1이 될 때까지 출력을 하겠다.
+        }   // i가 n - 1이 되면 반복문을 빠져나간다.
     }
 }
