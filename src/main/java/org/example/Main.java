f006f3ab529c71b5674c3ed127c31d9578ad8f80
ryuj7338 <ryuj7338@gmail.com>
2025-03-17 18:12:54 +0900

    백준 10998

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
 
-            Scanner sc= new Scanner(System.in);
+            Scanner sc= new Scanner(System.in);   // 입력
 
-            int A, B;
-            A = sc.nextInt();
-            B = sc.nextInt();
+            int A, B;   // int타입 A,B 선언
+            A = sc.nextInt();   // A의 값을 정수로 변환 / A = 1
+            B = sc.nextInt();   // B의 값을 정수로 변환 / B = 2
 
 
-            System.out.println(A+B);
+            System.out.println(A * B); //출력 : 2
         }
     }
 
