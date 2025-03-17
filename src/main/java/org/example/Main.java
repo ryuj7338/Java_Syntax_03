2171963221f4a1782d286d35a1e13e15dc58d11a
ryuj7338 <ryuj7338@gmail.com>
2025-03-17 19:00:50 +0900

    백준 10869

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,21 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner sc= new Scanner(System.in);   // 입력
+        Scanner sc= new Scanner(System.in);   // 입력
 
-            double A, B;   // int타입 A,B 선언
-            A = sc.nextDouble();   // A = 1
-            B = sc.nextDouble();   // B = 3
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
 
-//            float A, B;
-//            A = sc.nextFloat();
-//            B = sc.nextFloat();     // 출력 값 :0.33333334
-
-
-            System.out.println(A / B); //출력 : 0.33333333333333333333333333333333
-        }
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
     }
+}
 
 
 
