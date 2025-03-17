173d153f1457e06ab6f74ed55b717f10ea1eb092
ryuj7338 <ryuj7338@gmail.com>
2025-03-17 19:42:22 +0900

    백준 2588

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
         // % - 나머지
         // / - 몫
-        System.out.println(A *(B%10));
-        System.out.println(A *((B%100)/10));
-        System.out.println(A * (B/100));
+        System.out.println(A *(B%10));  // B%10한 이유? 1의 자리수를 얻기 위해
+        System.out.println(A *((B%100)/10));    // 1. B%100한 이유? 10의 자리수 얻기위해 2. ((B%10)/10)한 이유? 10의 자리수 중에 1의 자리를 제외하기 위해서
+        System.out.println(A * (B/100));    // B/100한 이유? 100의 자리수를 얻기 위해
         System.out.println(A * B);
     }
 }
