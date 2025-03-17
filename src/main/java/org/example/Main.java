0ed0cc5a81b87f4c1ee4607413da178b0046b75c
ryuj7338 <ryuj7338@gmail.com>
2025-03-17 20:13:44 +0900

    백준 11382

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,24 @@ public class Main {
 
         Scanner sc= new Scanner(System.in);   // 입력
 
-       int A = sc.nextInt();    // 472
-       int B = sc.nextInt();    // 385
+        String[] num = sc.nextLine().split(" ");
 
-        // % - 나머지
-        // / - 몫
-        System.out.println(A *(B%10));  // B%10한 이유? 1의 자리수를 얻기 위해
-        System.out.println(A *((B%100)/10));    // 1. B%100한 이유? 10의 자리수 얻기위해 2. ((B%10)/10)한 이유? 10의 자리수 중에 1의 자리를 제외하기 위해서
-        System.out.println(A * (B/100));    // B/100한 이유? 100의 자리수를 얻기 위해
-        System.out.println(A * B);
+        long A = Long.parseLong(num[0]);
+        long B = Long.parseLong(num[1]);
+        long C = Long.parseLong(num[2]);
+
+        System.out.println(C +B+C);
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        long A, B, C;
+//
+//        A = sc.nextLong();
+//        B = sc.nextLong();
+//        C = sc.nextLong();
+//
+//
+//        System.out.println(A+B+C);
     }
 }
 
