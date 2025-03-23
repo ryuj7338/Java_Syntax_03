8119d9cd451efebc85f6ccebad3c471c1d65006f
ryuj7338 <ryuj7338@gmail.com>
2025-03-23 20:38:06 +0900

    백준 1330

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,76 +1,29 @@
 package org.example;
 
-// 1부터 n까지 소수의 갯수를 출력하는 함수 만들기
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
-        a사람인력관리소.add사람("홍길순", 33);
-        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
-        a사람인력관리소.add사람("홍길동", 20);
-        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
-        a사람인력관리소.add사람("임꺽정", 30);
-        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
+        Scanner sc = new Scanner(System.in);
 
-        사람 a사람1 = a사람인력관리소.get사람(1);
-        a사람1.자기소개();
-        // 저는 1번, 홍길순, 33살 입니다.
+        String[] numbers = sc.nextLine().split(" ");
 
-        사람 a사람2 = a사람인력관리소.get사람(2);
-        a사람2.자기소개();
-        // 저는 2번, 홍길동, 20살 입니다.
+        int A = Integer.parseInt(numbers[0]);
+        int B = Integer.parseInt(numbers[1]);
 
-        사람 a사람3 = a사람인력관리소.get사람(3);
-        a사람3.자기소개();
-        // 저는 3번, 임꺽정, 30살 입니다.
-    }
-}
-class 사람인력관리소 {
-    사람 a사람1;
-    사람 a사람2;
-    사람 a사람3;
+//        int A, B;
+//
+//        A = sc.nextInt();
+//        B = sc.nextInt();
 
-    int 마지막_사람_번호 = 0;
-
-    void add사람(String 이름, int 나이) {
-        int 번호 = 마지막_사람_번호 + 1;
-        사람 a사람 = new 사람();
-        a사람.번호 = 마지막_사람_번호 + 1;
-        a사람.이름 = 이름;
-        a사람.나이 = 나이;
-
-        if (번호 == 1) {
-            a사람1 = a사람;
-        } else if (번호 == 2) {
-            a사람2 = a사람;
-        } else if (번호 == 3) {
-            a사람3 = a사람;
+        if (A > B) {
+            System.out.print(">");
+        }else if (A < B) {
+            System.out.print("<");
+        }else if (A == B) {
+            System.out.print("==");
         }
-
-
-        System.out.printf("나이가 %d살인 %d번째 사람(%s)이 추가되었습니다.\n", 나이, 번호, 이름 );
-        마지막_사람_번호++;
-
-
-    }
-    사람 get사람(int 번호) {
-        if (번호 == 1) {
-            return a사람1;
-        } else if (번호 == 2) {
-            return a사람2;
-        } else if (번호 == 3) {
-            return a사람3;
-        }
-        return null;
-    }
-}
-class 사람 {
-    int 번호;
-    String 이름;
-    int 나이;
-
-    void 자기소개() {
-        System.out.println("저는" + 번호 +"번, "+ 이름 + ", " + 나이 + "살 입니다.");
 
     }
 }
