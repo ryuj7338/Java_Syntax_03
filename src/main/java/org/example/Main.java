8f4bec546e0decb12c2e3d8aac45ecb45759cc3e
ryuj7338 <ryuj7338@gmail.com>
2025-03-23 23:12:50 +0900

    백준 10950 - 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
-        int arr[] = new int[T];
 
-        for (int i = 1; i < T; i++) {
+        for (int i = 1; i <= T; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-
-            arr[i -1] = a + b;
-            System.out.println(arr[i-1]);
+            System.out.println(a + b);
         }
 
     }
