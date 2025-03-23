7f9e3aed5c87d6f97511b234462f844052d22f0f
ryuj7338 <ryuj7338@gmail.com>
2025-03-23 23:26:25 +0900

    백준 10950 - 1 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
+        int[] arr = new int[T];
 
-        for (int i = 1; i <= T; i++) {
+        for (int i = 0; i < T; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            System.out.println(a + b);
+
+            arr[i] = a + b;
+
+        }
+        for (int i = 0; i < T; i++) {
+            System.out.println(arr[i]);
         }
 
     }
